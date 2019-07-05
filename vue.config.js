7faf1b27f64f0b84db8c155de86e5f8489cb7eaa
系统开发部 <xitongkaifabu@Mac-mini.local>
2019-07-05 18:27:08 +0800

    eslint

--- vue.config.js
+++ vue.config.js
@@ -1,36 +1,36 @@
 module.exports = {
-    pages: {
-        xuanshou: {
-            // 应用入口配置，相当于单页面应用的main.js，必需项
-            entry: 'src/pages/xuanshou/xuanshou.js',
+  pages: {
+    xuanshou: {
+      // 应用入口配置，相当于单页面应用的main.js，必需项
+      entry: 'src/pages/xuanshou/xuanshou.js',
 
-            // 应用的模版，相当于单页面应用的public/index.html，可选项，省略时默认与模块名一致
-            template: 'public/index.html',
+      // 应用的模版，相当于单页面应用的public/index.html，可选项，省略时默认与模块名一致
+      template: 'public/index.html',
 
-            // 编译后在dist目录的输出文件名，可选项，省略时默认与模块名一致
-            filename: 'xuanshou.html',
+      // 编译后在dist目录的输出文件名，可选项，省略时默认与模块名一致
+      filename: 'xuanshou.html',
 
-            // 标题，可选项，一般情况不使用，通常是在路由切换时设置title
-            // 需要注意的是使用title属性template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-            title: 'xuanshou',
+      // 标题，可选项，一般情况不使用，通常是在路由切换时设置title
+      // 需要注意的是使用title属性template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'xuanshou',
 
-            // 包含的模块，可选项
-            chunks: ['xuanshou']
-        },
-        zhuchi: {
-            entry: 'src/pages/zhuchi/zhuchi.js',
-            template: 'public/index.html',
-            filename: 'zhuchi.html',
-            title: 'zhuchi',
-            chunks: ['zhuchi']
-        },
-        screen: {
-            entry: 'src/pages/screen/screen.js',
-            template: 'public/index.html',
-            filename: 'screen.html',
-            title: 'screen',
-            chunks: ['screen']
-        }
+      // 包含的模块，可选项
+      chunks: ['xuanshou']
     },
-    productionSourceMap: false
+    zhuchi: {
+      entry: 'src/pages/zhuchi/zhuchi.js',
+      template: 'public/index.html',
+      filename: 'zhuchi.html',
+      title: 'zhuchi',
+      chunks: ['zhuchi']
+    },
+    screen: {
+      entry: 'src/pages/screen/screen.js',
+      template: 'public/index.html',
+      filename: 'screen.html',
+      title: 'screen',
+      chunks: ['screen']
+    }
+  },
+  productionSourceMap: false
 }
