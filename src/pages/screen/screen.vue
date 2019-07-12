60938b6d0136300c467a52093fe5283bb2d96797
karroy0715 <807536400@qq.com>
2019-07-12 13:20:29 +0800

    实现刮刮卡效果（存在部分问题）

--- src/pages/screen/screen.vue
+++ src/pages/screen/screen.vue
@@ -6,23 +6,16 @@
       <el-col :span="1" >
         <img class="header-img" src="./images/logo.png">
       </el-col>
-      <el-col :span="15" class="header-left">
+      <el-col :span="16" class="header-left">
         <div class="header-text">{{raceName}}</div>
       </el-col>
-      <el-col :span="6" class="header-right">
+      <el-col :span="7" class="header-right">
         <el-row class="header-right-top">
           <el-col :span="12">{{apartment}}</el-col>
-          <el-col :span="12">
-            <el-date-picker
-              class = "data-picker"
-              v-model="value1"
-              type="date"
-              placeholder="选择日期">
-            </el-date-picker>
-          </el-col>
+          <el-col :span="12">{{currentDate}}</el-col>
         </el-row>
         <el-row class="header-right-bottom">
-          <el-col :span="12">{{useTime}}</el-col>
+          <el-col :span="12" class="race-time">{{useTime}}</el-col>
           <el-col :span="12" class="showtime">
             <span class="ues-time">{{minute}}</span>:<span class="ues-time">{{second}}</span></el-col>
         </el-row>
@@ -36,9 +29,24 @@
       <el-row :span="4" class="border title title-question">
         <svg-icon icon-class="file"></svg-icon>题目:{{currentNum}}
       </el-row>
-      <el-row :span="18" class="border opacity question"><div v-html="question"></div></el-row>
+      <el-row :span="18" id="question-sizeof" class="border opacity question"><div v-html="question"></div></el-row>
       <el-row :span="2" class="border opacity correct-answer">
-        <svg-icon icon-class="showanswer"></svg-icon>正确答案：<span v-show="showAnswer">{{correctAnswer}}</span></el-row>
+        <el-col :span="7" class="answer-icon"><svg-icon icon-class="showanswer"></svg-icon>正确答案：</el-col>
+        <el-col :span="10" v-show="showAnswer">
+          <vue-scratch-card
+            class="scratch-card"
+            element-id='scratchWrap1'
+            :ratio='0.8'
+            :move-radius='25'
+            :start-callback='startCallback'
+            :clear-callback='clearCallback'
+            result-img=''
+            cover-color='#caa'>
+            <div class="scratch-card-result" slot='result'>{{correctAnswer}}</div>
+              <!-- <slot >{{correctAnswer}}</slot> -->
+          </vue-scratch-card>
+        </el-col>
+      </el-row>
     </div>
     </el-col>
     <!-- 各组亮灯 组名 答案 时间 超时 分数情况 -->
@@ -54,19 +62,16 @@
         <el-table-column
           type="index"
           label="排名"
-          width="250px"
+          width="200px"
           align="center">
         </el-table-column>
         <el-table-column
-          width="120px">
-          <template>
-            <svg-icon icon-class="img"></svg-icon>
-          </template>
-        </el-table-column>
-        <el-table-column
-          prop="name"
           label="组名"
           align="center">
+          <template slot-scope="scope">
+            <svg-icon style="float:left" icon-class="img"></svg-icon>
+            <span>{{scope.row.name}}</span>
+          </template>
         </el-table-column>
         <el-table-column
           class="bg-green"
@@ -75,11 +80,18 @@
           align="center">
         </el-table-column>
         <el-table-column
-          prop="answer"
           label="答案"
           align="center">
-          <template>
-
+          <template slot-scope="scope">
+            <vue-scratch-card
+              class="scratch-card2"
+              element-id='scratchWrap2'
+              :ratio='0.9'
+              :move-radius='15'
+              result-img=''
+              cover-color='#cca'>
+              <span class="scratch-card-result2" slot='result'>{{scope.row.answer}}</span>
+            </vue-scratch-card>
           </template>
         </el-table-column>
         </el-table>
@@ -95,6 +107,7 @@ export default {
   data() {
     return {
       apartment: '',
+      currentDate: '2019-07-15',
       useTime: '比赛用时',
       minute: '',
       second: '',
@@ -106,42 +119,20 @@ export default {
       showAnswer: true,
       question: '中国共产党性质是什么？<br /> A、中国共产党是中国工人阶级的先锋队<br /> B 中国人民和中华民族的先锋队<br /> C 中国特色社会主义事业的领导核心<br /> D 代表中国先进生产力的发展要求，代表中国先进文化的前进方向，代表中国最广大人民的根本利益',
       tableData: [{}],
-      backImage: {
+      backImage: { // 我放在css里面，怎么都不行
         backgroundImage: 'url(' + require('./images/back.png') + ')',
         backgroundRepeat: 'no-repeat',
         backgroundSize: '100% 100%'
-      },
-      pickerOptions: {
-        disabledDate(time) {
-          return time.getTime() > Date.now()
-        },
-        shortcuts: [{
-          text: '今天',
-          onClick(picker) {
-            picker.$emit('pick', new Date())
-          }
-        }, {
-          text: '昨天',
-          onClick(picker) {
-            const date = new Date()
-            date.setTime(date.getTime() - 3600 * 1000 * 24)
-            picker.$emit('pick', date)
-          }
-        }, {
-          text: '一周前',
-          onClick(picker) {
-            const date = new Date()
-            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7)
-            picker.$emit('pick', date)
-          }
-        }]
-      },
-      value1: '',
-      value2: ''
+      }
     }
   },
   methods: {
-    // 从后台传过来的数据
+    startCallback() {
+      console.log('抽奖成功！')
+    },
+    clearCallback() {
+      console.log('清除完毕！')
+    },
     tableRowClassName({row, rowIndex}) {
       if (rowIndex % 2 === 0) {
         return 'warning-row'
@@ -173,6 +164,7 @@ export default {
       this.correctAnswer = data.config.correctAnswer
       this.raceName = data.config.info[0].raceName
       this.apartment = data.config.info[0].apartment
+      this.currentDate = data.config.info[0].currentDate
       this.currentNum = data.config.currentNum
       this.question = data.config.question
       this.showAnswer = data.config.showAnswer
@@ -200,49 +192,72 @@ export default {
         config: {
           info: [
             {
-              raceName: '践行社会主核心价值观你追我赶之知识竞赛',
-              apartment: '网络软件研发部'
+              raceName: '践行社会主核心价值观知识竞赛',
+              apartment: '网络软件研发部部门',
+              currentDate: '2019-07-12'
             }
           ],
           beginTime: 0,
           // beginTime: Date.now(), // 时间戳，后台返回的
           currentNum: 66,
           question: '中国共产党性质是什么？<br /> A、中国共产党是中国工人阶级的先锋队<br /> B 中国人民和中华民族的先锋队<br /> C 中国特色社会主义事业的领导核心<br /> D 代表中国先进生产力的发展要求，代表中国先进文化的前进方向，代表中国最广大人民的根本利益',
-          correctAnswer: 'ABCE',
+          correctAnswer: 'ABCDE',
           showAnswer: true, // 主持界面点击显示答案时传过来的值
           firstAnswer: [{teamToken: '6666', answer: 'ABC'}],
-          allAnswer: [{teamToken: '', answer: ''}],
+          allAnswer: [
+            // {teamToken: '', answer: ''}
+            {
+              teamToken: '0921',
+              answer: 'A'
+            },
+            {
+              teamToken: '1108',
+              answer: 'AB'
+            },
+            {
+              teamToken: '1128',
+              answer: 'ABC'
+            },
+            {
+              teamToken: '6666',
+              answer: 'ABCD'
+            },
+            {
+              teamToken: '0715',
+              answer: 'ABCDE'
+            }
+          ],
           tableData: [
             // {teamToken: '', name: '', score: 0, answer: ''}
             {
               teamToken: '0921',
               name: '小分队',
               score: '53',
-              answer: 'A'
+              answer: ''
             },
             {
               teamToken: '1108',
               name: '加油队123',
               score: '45',
-              answer: 'AB'
+              answer: ''
             },
             {
               teamToken: '1128',
               name: '强强联盟',
               score: '20',
-              answer: 'ABC'
+              answer: ''
             },
             {
               teamToken: '6666',
               name: '薛定谔的猫',
               score: '45',
-              answer: 'ABCD'
+              answer: ''
             },
             {
               teamToken: '0715',
               name: '@未来可期 事无蹉跎',
               score: '20',
-              answer: 'ABCDE'
+              answer: ''
             }
           ]
         }
@@ -252,6 +267,7 @@ export default {
   mounted() {
     this.login()
     this.computeTime() // 这有个问题就是数据不能实时显示。
+    // this.sizeof(this.question, 'utf-8')
   },
   watch: {
     // 作用：监听用时变化
@@ -281,14 +297,14 @@ export default {
 }
 .header-main{
   height: 15vh;
-  margin: 50px 0px 50px 100px;
+  margin: 50px 50px 50px 50px;
 }
 .header-left{
   font-size: 100px;
   padding: 0px;
 }
 .header-right{
-  font-size: 60px;
+  font-size: 50px;
   padding: 50px 50px;
 }
 .header-img{
@@ -300,9 +316,8 @@ export default {
   display: inline;
   line-height: 15vh;
 }
-.el-input__inner{
-  width: 260px!important;
-  height: 80px!important;
+.race-time{
+  margin-top: 2vh;
 }
 .showtime{
   font-size: 100px;
@@ -313,7 +328,7 @@ export default {
   font-size: 70px;
 }
 .grid-content{
-  margin: 20px 40px auto 40px;
+  margin: 60px 40px auto 40px;
 }
 .border{
   border:1px solid #CD2323;
@@ -333,21 +348,22 @@ export default {
   background:rgba(255,255,255,0.1);
 }
 .question{
-  font-size: 50px;
   height: 50vh;
+  font-size: 50px;
   line-height: 100px;
   padding: 80px 100px;
   margin-bottom: 3vh;
 }
 .svg-icon{
   margin-right: 20px;
-  margin-left:20px;
+  margin-left:40px;
   font-size: 80px;
 }
 .correct-answer{
+  padding: 0px;
   border-radius: 25px;
   height: 10vh;
-  font-size: 70px;
+  font-size: 50px;
 }
 .right-bottom{
   /* font-family:sans-serif; */
@@ -378,5 +394,24 @@ export default {
 }
 th{
   height: 150px;
+}
+.answer-icon{
+  line-height: 4;
+}
+.scratch-card{
+  width: 500px!important;
+  height: 10vh!important;
+}
+.scratch-card2{
+  width: 300px!important;
+  height: 5vh!important;
+}
+.scratch-card-result2{
+  text-align: center;
+}
+.scratch-card-result{
+  text-align: center;
+  line-height: 2;
+  font-size: 100px;
 }
 </style>
